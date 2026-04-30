7d01d6427374856b02a4b362fcde10ce12b92774
mariaiescalante <mariaisabel.escalantej@gmail.com>
2026-04-30 14:52:34 -0400

    cambios

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-import dotenv from "dotenv";
-dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import './src/config/bd.js'; // Asegúrate de que el archivo se llame bd.js o db.js
 
+// Inicializamos la aplicación
+const app = express();
 
-import app from "./app.js";
-import dotenv from "dotenv";
+// Middlewares
+app.use(cors()); 
+app.use(morgan('dev')); 
+app.use(express.json()); 
 
-dotenv.config();
+// Ruta de prueba inicial
+app.get('/', (req, res) => {
+    res.json({ mensaje: '¡El backend de uptnt manuela saenz está vivo y funcionando!' });
+});
 
-const PORT = process.env.PORT || 3001;
+// Definimos el puerto
+const PORT = 3000;
 
+// Encendemos el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
--- src/config/bd.js
+++ src/config/bd.js
@@ -3,9 +3,6 @@ dotenv.config();
 
 
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const { Pool } = pkg;
 
